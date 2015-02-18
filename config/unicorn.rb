56fdfacfcc42688ea71da06d7dbf60be94f97a8b
Jason Hang <jason.d.hang@gmail.com>
2015-02-17 23:58:35 -0500

    Found a typo in the unicorn file that was causing an error

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,7 +17,7 @@ after_fork do |server, worker|
   Sidekiq.configure_client do |config|
     config.redis = { :size => 1 }
   end
-  Side kiq.configure_server do |config|
+  Sidekiq.configure_server do |config|
     config.redis = { :size => 5 }
   end
 
